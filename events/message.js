70db36c904511638aef757a276cbf8ef7496db95
Edouard Courty <edouard.courty2gmail.com>
2020-05-06 01:09:09 +0200

    fix(event)refactor-message-event

--- events/message.js
+++ events/message.js
@@ -1,29 +1,26 @@
 const utils = require("../lib/utils");
-const fs = require("fs");
+const Whitelist = require("../models/Whitelist");
 
 module.exports = async (client, message) => {
+  if (message.author.bot) return;
+  const whitelistedServerIds = (await Whitelist.findOne()).servers
   // Listening condition
-  message.isListened = true;
   message.isCommand  = message.content.startsWith(process.env.COMMAND_PREFIX) ? 1 : 0;
   let userIsAdmin = message.member.hasPermission("ADMINISTRATOR");
-  // User-specific listening condition
-  const isSentByUser = message.author.bot === false;
+  if (!userIsAdmin) return;
 
+  let isServerWhitelisted = false;
   const serverId = parseInt(message.guild.id);
-  /** @type Array<Number> */
-  const authorizedwhitelist = require("../config/whitelist").ids;
-  const isServerAuthorized = authorizedwhitelist.includes(serverId);
+  if (whitelistedServerIds.includes(serverId)) isServerWhitelisted = true;
 
-  if (!message.isListened && !message.isCommand) return;
+  if (!message.isCommand) return;
 
-  message.args        = message.content.split(" ");
-  message.fullArgs    = message.args;
-  message.commandName = message.isCommand ? message.args.shift().slice(1).toLowerCase() : undefined;
-  message.baseContent = message.isCommand ? message.content : undefined;
-  message.content     = message.isCommand ? message.args.join(" ") : message.content;
+  message.args = message.content.split(" ");
+  message.fullArgs = message.args;
+  message.commandName = message.args.shift().slice(1).toLowerCase();
+  message.baseContent = message.content;
+  message.content = message.args.join(" ");
 
-  if (message.isCommand && isSentByUser && userIsAdmin) {
-    await utils.runCommand(client, message, isServerAuthorized);
-  }
+  await utils.runCommand(client, message, isServerWhitelisted);
   utils.log(message);
 };
